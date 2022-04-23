31c4bc67f5fb20f5f48b640e8e78b1504abc30b2
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-04-23 20:22:16 +0700

    #7 working on calculateSum() function

--- Projects/EventHandler/app/src/main/java/com/example/eventhandler/MainActivity.java
+++ Projects/EventHandler/app/src/main/java/com/example/eventhandler/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,24 +14,33 @@ public class MainActivity extends AppCompatActivity {
     // Bước 1: Khai báo biến đối tượng muốn xử lý logic
     // Bước 2: Mapping đối tượng vừa khai báo đến thằng thư mục layout thông qua method findViewById()
     // Bước 3:
+    TextView tvSum;
+    TextView tvMax;
+    TextView tvMin;
     EditText edtNumberA;
     EditText edtNumberB;
     EditText edtNumberC;
-    EditText edtAge;
-    Button btnShow;
+    Button btnEvaluate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvSum = findViewById(R.id.tvSum);
+        tvMax = findViewById(R.id.tvMax);
+        tvMin = findViewById(R.id.tvMin);
         edtNumberA = findViewById(R.id.edt_A);
-        edtAge = findViewById(R.id.edt_age);
-        btnShow = findViewById(R.id.btn_show);
+        edtNumberB = findViewById(R.id.edt_B);
+        edtNumberC = findViewById(R.id.edt_C);
+        btnEvaluate = findViewById(R.id.btn_evaluate);
 
-        btnShow.setOnClickListener(new View.OnClickListener() {
+        public double calculateSum()
+
+        btnEvaluate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String name = edtName.getText().toString().trim();
                 String age = edtAge.getText().toString().trim();
 
