b11154be1f42ac660279c1d46129bdae55213c95
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-04-23 22:50:19 +0700

    #8 before test 1

--- Projects/EventHandler/app/src/main/java/com/example/eventhandler/MainActivity.java
+++ Projects/EventHandler/app/src/main/java/com/example/eventhandler/MainActivity.java
@@ -5,9 +5,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
     EditText edtNumberC;
     Button btnEvaluate;
 
+    public double calculateSum(double numberA, double numberB, double numberC) {
+        return numberA + numberB + numberC;
+    }
+
+    public double maxValue(List<Double> array) {
+        double max = Collections.max(array);
+        return max;
+    }
+
+    public double minValue(List<Double> array) {
+        double min = Collections.min(array);
+        return min;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,17 +52,22 @@ public class MainActivity extends AppCompatActivity {
         edtNumberB = findViewById(R.id.edt_B);
         edtNumberC = findViewById(R.id.edt_C);
         btnEvaluate = findViewById(R.id.btn_evaluate);
-
-        public double calculateSum()
+        List<Double> numberArray= new ArrayList<Double>();
 
         btnEvaluate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String name = edtName.getText().toString().trim();
-                String age = edtAge.getText().toString().trim();
+                double numberA = Double.parseDouble(edtNumberA.getText().toString().trim());
+                double numberB = Double.parseDouble(edtNumberB.getText().toString().trim());
+                double numberC = Double.parseDouble(edtNumberC.getText().toString().trim());
+                numberArray.add(numberA);
+                numberArray.add(numberB);
+                numberArray.add(numberC);
 
-                Toast.makeText(getApplicationContext(), name + " " + age, Toast.LENGTH_SHORT).show();
+                tvSum.setText(String.valueOf(calculateSum(numberA, numberB, numberC)));
+                tvMax.setText(String.valueOf(maxValue(numberArray)));
+                tvMax.setText(String.valueOf(minValue(numberArray)));
             }
         });
     }
